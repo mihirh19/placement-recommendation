b86e2a27bbe893e2d04bd311128cc1d3fbb163db
mihirh19 <miheerhadvani990@gmail.com>
2023-12-27 08:57:16 +0530

    swr with client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,21 @@
-
+'use client'
 import Image from 'next/image'
 import { PrismaClient } from "@prisma/client";
 import axios from 'axios';
+import useSWR, { unstable_serialize } from 'swr';
+const fetcher = (...args) => fetch(...args).then(res => res.json())
+// export const getData = async () => {
+//   // const { data } = await axios.get(`${process.env.API_URL}/api`)
 
-export const revalidate = 0;
-
-// async function getData() {
-//   'use server'
-//   const prisma = new PrismaClient();
-//   const users = await prisma.user.findMany();
-//   return users;
+//   return data
 // }
-
-export const getData = async () => {
-  const { data } = await axios.get(`${process.env.API_URL}/api`)
-  return data
-}
-export default async function Home() {
-  const data = await getData();
+export default function Home() {
+  const { data } = useSWR('/api', fetcher)
   return (
     <div>
       <h1>Users</h1>
       <ul>
-        {data.map((user) => (
+        {data && data.map((user) => (
           <li key={user.id}>
             {user.name}
           </li>
