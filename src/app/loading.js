7af53f4154923a9c3996c0bc92cdd2374677aca2
mihirh19 <miheerhadvani990@gmail.com>
2024-01-04 19:33:14 +0530

    loading page added

---
+++ src/app/loading.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../app/login/page.module.css'
+const loading = () => {
+   return (
+      <div class="relative">
+
+
+         <div class="absolute bg-white bg-opacity-60 z-10 h-full w-full flex items-center justify-center">
+            <div class="flex items-center">
+               <span class="text-3xl mr-4">Loading</span>
+               <svg class="animate-spin h-8 w-8 text-gray-800" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 24 24">
+                  <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                  <path class="opacity-75" fill="currentColor"
+                     d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                  </path>
+               </svg>
+            </div>
+         </div>
+
+
+         <form className={styles.bg} >
+            <div className={styles.loginContainer} style={{ opacity: 1 }}>
+               <div className="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
+                  <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                     <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Login into your account
+                     </p><div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900">
+                           Your username
+                        </label>
+                        <input
+                           onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
+                           placeholder="JohnDoe" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" id="username" type="text" />
+                     </div>
+                     <div>
+                        <label className="block mb-2 text-sm font-medium text-gray-900">
+                           Password
+                        </label>
+                        <input
+                           onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
+                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" placeholder="••••••••" id="password" type="password" />
+                     </div>
+
+
+                     <button onClick={handleSubmit} className="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white" type="submit">
+                        Login
+                     </button>
+                  </div>
+               </div>
+            </div></form>
+      </div >
+   )
+}
+
+export default loading
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,6 +2,7 @@
 
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 export default function UserInfo({ username, email }) {
 
    const router = useRouter();
@@ -16,6 +17,16 @@ export default function UserInfo({ username, email }) {
                Email : <span className="font-bold">{email}</span>
             </div>
             <button onClick={() => signOut({ redirect: false }).then(() => {
+               toast.info('Sign Out Success', {
+                  position: "top-center",
+                  autoClose: 1500,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "dark",
+               });
                router.push("/login")
             })} className="bg-red-500 text-white font-bold px-6 py-2 mt-3">
                Log Out
