13c3863b9687bcdbc54ef3d8c70c557fe78a14a9
InshiyaRavat <insravat@gmail.com>
2024-01-04 15:22:33 +0530

    changes made in login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
+import styles from './page.module.css'
 
 const Login = () => {
   return (
-    <form>
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
+    <form className={styles.bg}>
+      <div className={styles.loginContainer} class="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0" style={{opacity:1}}>
         <div class="w-full bg-white rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <p class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Create an account
-            
-            
+              Login into your account            
               </p><div>
                 <label class="block mb-2 text-sm font-medium text-gray-900">
                   Your username
@@ -21,12 +20,6 @@ const Login = () => {
                   Password
                 </label>
                 <input class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" placeholder="••••••••" id="password" type="password"/>
-              </div>
-              <div>
-                <label class="block mb-2 text-sm font-medium text-gray-900">
-                  Confirm password
-                </label>
-                <input class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" placeholder="••••••••" id="confirmPassword" type="password"/>
               </div>
               <div class="flex items-start">
                 <div class="flex items-center h-5">
@@ -43,9 +36,8 @@ const Login = () => {
               </div>
 
               <button class="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-white" type="submit">
-                Create an account
-              </button>
-            
+                Login
+              </button>            
           </div>
         </div>
       </div></form>
