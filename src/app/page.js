01959bd17d8dc296e60939f3046caf9759f7f2cc
mihirh19 <miheerhadvani990@gmail.com>
2024-01-04 19:08:47 +0530

    Login

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 
 import GetUser from '../components/GetUser';
+import Link from 'next/link'
 export default function Home() {
-  
+
 
   return (
     <div>
       <GetUser />
+      <Link href={'/login'}>
+        Login
+      </Link>
     </div>
   )
 }
