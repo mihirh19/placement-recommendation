f19c6712bb279e7ec15ec45e3fc760d8d7d392b3
mihirh19 <miheerhadvani990@gmail.com>
2024-01-10 09:46:57 +0530

    Register With Date Component is added

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -4,6 +4,12 @@ import React, { useState } from 'react';
 import styles from '../app/register/page.module.css';
 import axios from 'axios';
 import { toast } from "react-toastify";
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { CiCalendarDate } from "react-icons/ci";
+import { MdOutlineClear } from "react-icons/md";
+
 
 const RegisterUser = () => {
   const [userInfo, setUserInfo] = useState({
@@ -11,7 +17,7 @@ const RegisterUser = () => {
     password: '',
     studentid: '',
     emailaddress: '',
-    dateofbirth: '',
+    dateofbirth: new Date(),
     phonenumber: '',
   });
 
@@ -119,16 +125,34 @@ const RegisterUser = () => {
               {details.map((detail, index) => (
                 <div key={index} className={styles.formField}>
                   <div className={styles.formLabel}>{detail}</div>
-                  <input
-                    value={userInfo[detail.toLowerCase().replace(' ', '')]}
-                    onChange={(e) =>
-                      setUserInfo({ ...userInfo, [detail.toLowerCase().replace(' ', '')]: e.target.value })
-                    }
-                    placeholder={`Enter ${detail}`}
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
-                    id={detail.toLowerCase().replace(' ', '-')}
-                    type={detail.toLowerCase().includes('password') ? 'password' : 'text'}
-                  />
+                  {detail.toLowerCase().includes('dateofbirth') ? (
+                    <DatePicker
+                      calendarIcon={<CiCalendarDate />}
+                      clearIcon={<MdOutlineClear />}
+                      // format="dd-MM-y"
+                      maxDate={new Date()}
+                      value={userInfo.dateofbirth}
+                      onChange={(date) =>
+                        setUserInfo({ ...userInfo, dateofbirth: date })
+                      }
+                      placeholderText={`Enter ${detail}`}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                      id={detail.toLowerCase().replace(' ', '-')}
+                    />
+
+                  ) : (
+                    <input
+                      value={userInfo[detail.toLowerCase().replace(' ', '')]}
+                      onChange={(e) =>
+                        setUserInfo({ ...userInfo, [detail.toLowerCase().replace(' ', '')]: e.target.value })
+                      }
+                      placeholder={`Enter ${detail}`}
+                      className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                      id={detail.toLowerCase().replace(' ', '-')}
+                      type={detail.toLowerCase().includes('password') ? 'password' : 'text'}
+                    />
+                  )}
+
                 </div>
               ))}
             </div>
