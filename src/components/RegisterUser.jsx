a48a33dc6f85b2f7a317f26fe655fa5769b61e8b
InshiyaRavat <insravat@gmail.com>
2024-01-12 20:07:27 +0530

    changed color of register button

--- src/components/RegisterUser.jsx
+++ src/components/RegisterUser.jsx
@@ -192,7 +192,7 @@ const RegisterUser = () => {
 
             <Button
               onClick={handleSubmit}
-              color='primary'
+              style={{ backgroundColor: '#F55734', color: '#FFFFFF' }}
               className="w-full bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:ring-blue-800 text-white"
               type="submit"
             >
