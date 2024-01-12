5b7fe61105df5d061cf35bbf9c6d119fbe1b5d0e
InshiyaRavat <insravat@gmail.com>
2024-01-12 22:57:48 +0530

    navbar set according to student and admin role

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 'use client'
 import { Inter } from 'next/font/google'
+import { useSession } from "next-auth/react";
 import { SessionProvider } from 'next-auth/react'
 import './globals.css'
 import Navbar from '@/components/Navbar';
@@ -9,13 +10,17 @@ import { NextUIProvider } from "@nextui-org/react";
 
 
 export default function RootLayout({ children }) {
+  // const { status, data } = useSession();
   return (
     
     <SessionProvider>
-
+       
       <html lang="en">
         <body>
-        <Navbar userRole={'STUDENT'}/>
+        <NavbarWrapper />
+          {/* {status === "authenticated" && data?.role === "ADMIN" && (<Navbar userRole={'ADMIN'}/>)}
+          {status === "authenticated" && data?.role === "STUDENT" && (<Navbar userRole={'STUDENT'}/>)} */}
+        
           <NextUIProvider>
             <ToastContainer
               position="top-center"
@@ -38,4 +43,23 @@ export default function RootLayout({ children }) {
 
 
   )
+}
+
+function NavbarWrapper() {
+  const { status, data } = useSession();
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      {status === "authenticated" && data?.role === "ADMIN" && (
+        <Navbar userRole={'ADMIN'} />
+      )}
+      {status === "authenticated" && data?.role === "STUDENT" && (
+        <Navbar userRole={'STUDENT'} />
+      )}
+    </>
+  );
 }
