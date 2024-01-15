79aa24c794938527e094c33ecf6a243d68ca4146
InshiyaRavat <insravat@gmail.com>
2024-01-15 17:58:05 +0530

    designed home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,22 @@
-
+'use client';
+import { Image } from "@nextui-org/react";
 import GetUser from '../components/GetUser';
-import Link from 'next/link'
+import styles from '../app/login/page.module.css'
+
 export default function Home() {
-
-
   return (
     <div>
-      
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.heading}>Connecting Talent with Tomorrow's Opportunities</div>
+          <div className={styles.desc}>Your gateway to career success! Explore job opportunities, predict your ideal company match, and gain insights from alumni reviews. Empower your journey with personalized guidance and community-driven wisdom.</div>
+        </div>
+        <div className={styles.image}>
+          <Image id={styles.img1} src="https://images.pexels.com/photos/4064230/pexels-photo-4064230.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          <Image id={styles.img2} src="https://images.pexels.com/photos/7944038/pexels-photo-7944038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+        </div>
+      </div>
       <GetUser />
     </div>
-  )
+  );
 }
