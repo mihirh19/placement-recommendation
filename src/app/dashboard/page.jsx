f7f0fc8f7692261de731ae043b0c0abbb3ab97fb
InshiyaRavat <insravat@gmail.com>
2024-02-14 23:48:33 +0530

    dashboard added

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import UserInfo from "../../components/UserInfo";
+import Dashboard from "@/components/Dashboard/Dashboard";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 export default function Dashboard() {
@@ -12,7 +13,8 @@ export default function Dashboard() {
       router.push("/login");
    }
    if (status === "authenticated") return (
-      <UserInfo username={data?.username} email={data?.email} />
+      // <UserInfo username={data?.username} email={data?.email} />
+      <Dashboard/>
    )
 
 }
