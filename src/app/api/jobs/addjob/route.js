bf2f96fe59bb6ebc8e6bd26a6e51d9994d6d5672
mihirh19 <miheerhadvani990@gmail.com>
2024-02-16 22:22:15 +0530

    crud api for job is added

---
+++ src/app/api/jobs/addjob/route.js
@@ -0,0 +1,46 @@
+import prisma from '@/lib/prisma';
+import { authOptions } from '../../auth/[...nextauth]/route';
+import { Role } from '@prisma/client';
+import { getServerSession } from 'next-auth';
+import { NextResponse } from 'next/server';
+export async function POST(req, res) {
+
+   const session = await getServerSession(authOptions);
+
+   if (session) {
+      if (session.role !== Role.ADMIN && session.role !== Role.RECRUITER) {
+         return NextResponse.json({ message: "you are not allowed to add jobs" }, { status: 401 })
+      }
+   }
+   else {
+      return NextResponse.json({ message: "you are not allowed to add jobs" }, { status: 401 })
+   }
+
+   try {
+      const { title, description, company, companyUrl, role, location, salary, skills } = await req.json();
+      await prisma.job.create({
+         data: {
+            title,
+            description,
+            company,
+            companyUrl,
+            role,
+            location,
+            salary,
+            skills,
+            User: {
+               connect: {
+                  id: session.id
+               }
+            }
+         }
+      })
+
+
+
+      return NextResponse.json({ message: "Job  Created" }, { status: 200 });
+   } catch (err) {
+      return NextResponse.json({ message: err.message }, { status: 500 });
+   }
+
+}
